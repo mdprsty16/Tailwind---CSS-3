7d4300fdf8636b0c57f0292dd32cd406b1276388
mdprstyo <prasetyodeko240@gmail.com>
2025-01-25 15:30:53 +0700

    Belajar Bang

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,3 +20,36 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 }); 
+
+// Close Hamburger
+
+window.addEventListener('click', function(e) {
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+}); 
+
+// Dark Mode Toggle
+
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if(darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+//Toogle Fixed Mode
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia
+        ('(prefers-color-scheme: dark)').matches)) {
+            darkToggle.checked = true;
+        } else {
+            darkToggle.checked = false
+        }
